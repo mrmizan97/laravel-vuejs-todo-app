194ba1fa73dd6533bc0c4f4b90a45dda6d3dfbc9
mizan-apexdmit <md.mizanur@apexdmit.com>
2025-05-12 01:43:19 +0600

    updated authentication

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,9 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias([
-            'token.veryfy' => VarifyToken::class
-        ]);
+       
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->render(function (NotFoundHttpException $e, Request $request) {
--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware(['auth:sanctum','token.veryfy'])->prefix('auth')->group(function () {
+Route::middleware(['auth:sanctum'])->prefix('auth')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
     Route::apiResource('todos', TodoController::class);
 });
